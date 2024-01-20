844e099e4f8e747ed72898cdbcbbc42165adf59e
wangzhipeng <zhipeng.wang@gct-china.com>
2024-01-20 15:21:14 +0800

    First_Commit

--- Flask-Blog-Api/api/__init__.py
+++ Flask-Blog-Api/api/__init__.py
@@ -12,8 +12,17 @@ else:
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 app.config['SECRET_KEY'] = secrets.token_hex(16)
+
+# 关闭数据库修改跟踪操作[提高性能]：
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# 开启输出底层执行的sql语句
+app.config['SQLALCHEMY_ECHO'] = True
+
+# 开启数据库的自动提交功能[一般不使用]
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
